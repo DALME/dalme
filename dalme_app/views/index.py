d19eb2bafc12699796a8c2e4e6ae2576c427b010
Gabe Pizzorno <pizzorno@fas.harvard.edu>
2021-02-12 09:00:14 -0500

    changes for testing new navigation UI

--- dalme_app/views/index.py
+++ dalme_app/views/index.py
@@ -7,7 +7,6 @@ from django.views.generic.base import TemplateView
 @method_decorator(login_required, name='dispatch')
 class Index(TemplateView, DALMEContextMixin):
     template_name = 'dalme_app/index.html'
-    breadcrumb = [('Dashboard', '')]
     page_title = 'Dashboard'
 
     def get_context_data(self, **kwargs):
