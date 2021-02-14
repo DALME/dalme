4529cf5d23e1e099550b3cafd3673303e4103c57
Gabe Pizzorno <pizzorno@fas.harvard.edu>
2021-02-14 13:31:23 -0500

    improved user and comment serialization for tickets

--- dalme_api/serializers/tickets.py
+++ dalme_api/serializers/tickets.py
@@ -2,16 +2,18 @@ from dalme_app.models import Ticket
 from rest_framework import serializers
 from dalme_api.serializers.others import TagSerializer
 from dalme_api.serializers.users import UserSerializer
+from dalme_api.serializers.comments import CommentSerializer
 
 
 class TicketSerializer(serializers.ModelSerializer):
     tags = TagSerializer(many=True, required=False)
-    creation_user = UserSerializer(fields=['full_name', 'username', 'id'])
+    creation_user = UserSerializer(fields=['full_name', 'username', 'id', 'avatar'])
+    modification_user = UserSerializer(fields=['full_name', 'username', 'id'])
 
     class Meta:
         model = Ticket
         fields = ('id', 'subject', 'description', 'status', 'tags', 'url', 'file',
-                  'creation_user', 'creation_timestamp')
+                  'creation_user', 'creation_timestamp', 'modification_user', 'modification_timestamp')
         extra_kwargs = {
             'tags': {'required': False}
             }
